d1f9146591fd0030b446524669c5bd0754f7c013
dheepakraj.selvakumar <dheepakraj.selvakumar@zohocorp.com>
2020-03-03 03:16:30 +0530

    recyclerView Adapter implemented

---
+++ app/src/main/java/com/zestworks/trendingrepositories/common/ListingAdapter.kt
@@ -0,0 +1,52 @@
+package com.zestworks.trendingrepositories.common
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.zestworks.data.model.TrendingDevelopers
+import com.zestworks.data.model.TrendingRepositories
+import com.zestworks.trendingrepositories.R
+import de.hdodenhof.circleimageview.CircleImageView
+
+class ListingAdapter(var listItems: List<Any>): RecyclerView.Adapter<ListingAdapter.Listholder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Listholder {
+        val view =
+            LayoutInflater.from(parent.context!!)
+                .inflate(R.layout.list_holder, parent, false)
+        return Listholder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return listItems.size
+    }
+
+    fun updateListItems(listItems: List<Any>) {
+        this.listItems = listItems
+        notifyDataSetChanged()
+    }
+
+    override fun onBindViewHolder(holder: Listholder, position: Int) {
+        val item = listItems[position]
+        if(item is TrendingRepositories) {
+            holder.title.text = item.name
+            holder.description.text = item.description
+            Glide.with(holder.imageView)
+                .load(item.avatar)
+                .placeholder(R.drawable.ic_launcher_background)
+                .into(holder.imageView)
+        } else if(item is TrendingDevelopers) {
+            holder.title.text = item.name
+            holder.description.text = item.username
+        }
+    }
+
+    inner class Listholder(itemView: View): RecyclerView.ViewHolder(itemView){
+        val title = itemView.findViewById<TextView>(R.id.title)
+        val description = itemView.findViewById<TextView>(R.id.description)
+        val imageView = itemView.findViewById<CircleImageView>(R.id.author_image)
+    }
+}
--- app/src/main/java/com/zestworks/trendingrepositories/repositories/ui/TrendingRepositoriesFragment.kt
+++ app/src/main/java/com/zestworks/trendingrepositories/repositories/ui/TrendingRepositoriesFragment.kt
@@ -1,25 +1,76 @@
 package com.zestworks.trendingrepositories.repositories.ui
 
-
 import android.os.Bundle
+import android.os.strictmode.Violation
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.zestworks.data.model.LCE
 
 import com.zestworks.trendingrepositories.R
+import com.zestworks.trendingrepositories.common.ListingAdapter
+import com.zestworks.trendingrepositories.repositories.viewmodel.TrendingRepositoriesViewModel
+import com.zestworks.trendingrepositories.repositories.viewmodel.ViewModelFactory
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.android.synthetic.main.fragment_trending_repositories.*
 
-/**
- * A simple [Fragment] subclass.
- */
 class TrendingRepositoriesFragment : Fragment() {
+
+    private lateinit var compositeDisposable: CompositeDisposable
+    private lateinit var trendingRepositoriesViewModel: TrendingRepositoriesViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_trending_repositories, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        repositories_shimmer.visibility = View.VISIBLE
+        repositories_error.visibility = View.GONE
+        repo_list_view.layoutManager = LinearLayoutManager(context)
+        repo_list_view.addItemDecoration(DividerItemDecoration(context, DividerItemDecoration.VERTICAL))
+        repo_list_view.adapter = ListingAdapter(listOf())
+    }
+
+    override fun onStart() {
+        super.onStart()
+        trendingRepositoriesViewModel = ViewModelFactory.getTrendingRepositoriesViewModel(activity as AppCompatActivity)
+        compositeDisposable = CompositeDisposable()
+        compositeDisposable.add(trendingRepositoriesViewModel.repoState.observeOn(AndroidSchedulers.mainThread()).subscribe {
+            when(it) {
+                is LCE.YetToMakeRequest -> {
+                    repositories_shimmer.visibility = View.VISIBLE
+                    repositories_error.visibility = View.GONE
+                }
+                is LCE.Loading -> {
+                    repositories_shimmer.visibility = View.VISIBLE
+                    repositories_error.visibility = View.GONE
+                }
+                is LCE.Content -> {
+                    repositories_shimmer.visibility = View.GONE
+                    repositories_error.visibility = View.GONE
+                    (repo_list_view.adapter as ListingAdapter).updateListItems(it.data)
+                }
+                is LCE.Error -> {
+                    repositories_shimmer.visibility = View.GONE
+                    repositories_error.visibility = View.VISIBLE
+                }
+            }
+        })
+        trendingRepositoriesViewModel.getTrendingRepos()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        compositeDisposable.dispose()
     }
 
 
